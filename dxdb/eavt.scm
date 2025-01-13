2c0787eae416c4295999869717fbeb2675953dac
Amirouche Amazigh BOUBEKKI <hello@amirouche.dev>
2025-01-13 18:12:17 +0100

    remove files from too much ambitious project

--- dxdb/eavt.scm
+++
@@ -1,9 +0,0 @@
-(library (letloop dxdb eavt)
-  (export make-eavt eavt-add! eavt-query eavt-query-at)
-  (import (chezscheme)
-          (letloop r999)
-          (letloop dxdb)
-          (letloop byter)
-          (letloop dxdb nstore))
-
-  (include "letloop/dxdb/eavt.body.scm"))
